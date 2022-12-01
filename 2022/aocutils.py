9472b03a11bee9b8b09579f23f19ede4b3c43bb0
Niklas Rousset <niklas.rousset@gmail.com>
2022-12-01 09:24:47 +0100

    2022 day 1 + gitignore update

---
+++ 2022/aocutils.py
@@ -0,0 +1,18 @@
+
+from typing import Any, Callable
+
+
+def read_file(filename: str) -> str:
+    return open(filename).read()
+
+def read_lines(filename: str, apply: Callable[[str], Any] | None = lambda x: x) -> list:
+    data = read_file(filename)
+    return [apply(l) for l in data.splitlines() if l != ""]
+
+def read_blocks(filename: str, block_sep: str = "\n\n", apply_on_block: Callable[[str], Any] = lambda x: x) -> list:
+    data = read_file(filename)
+    return [apply_on_block(b.strip()) for b in data.split(block_sep) if b != ""]
+
+def read_lineblocks(filename: str, block_sep: str = "\n\n", apply_on_line: Callable[[str], Any] | None = None) -> list:
+    data = read_file(filename)
+    return [[apply_on_line(l) for l in b.strip().split("\n") if l != ""] for b in data.split(block_sep) if b != ""]
