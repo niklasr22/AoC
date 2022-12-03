94437b4a2d5cb5a14af61ef0f952b3048c9633f9
Niklas Rousset <niklas.rousset@gmail.com>
2022-12-03 10:32:29 +0100

    day 3

--- 2022/day3a.py
+++ 2022/day3a.py
@@ -2,17 +2,17 @@ import aocutils
 
 lines = aocutils.read_lines("./2022/inputs/day3.txt")
 
-def get_prio(item):
+def get_prio(item: str) -> int:
     if ord(item) <= ord("Z"):
         return ord(item) - ord("A") + 27
     return ord(item) - ord("a") + 1
 
-def search_compartments(rucksack):
+def search_compartments(rucksack: str):
     first_compartment = rucksack[:len(rucksack)//2]
     second_compartment = rucksack[len(rucksack)//2:]
-    for c in first_compartment:
-        if c in second_compartment:
-            return get_prio(c)
+    for item in first_compartment:
+        if item in second_compartment:
+            return get_prio(item)
 
 priorities = [search_compartments(l) for l in lines]
 print(sum(priorities))
