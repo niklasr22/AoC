41fa6b3229bca6f599509f838fcb3865d8b308a2
Niklas Rousset <niklas.rousset@gmail.com>
2022-12-06 16:03:25 +0100

    day 6

---
+++ 2022/day6.py
@@ -0,0 +1,11 @@
+import aocutils
+
+input = aocutils.read_file("./2022/inputs/day6.txt")
+
+def first_occurence_of_x_distinct_chars(input: str, x: int):
+    for i in range(x,len(input)):
+        if len(set(input[i-x+1:i+1])) == x:
+            return i
+
+print("a", first_occurence_of_x_distinct_chars(input, 4))
+print("b", first_occurence_of_x_distinct_chars(input, 14))
