48fa3718280d6a90b9f8d5bdc6f11a0757a42702
Niklas Rousset <niklas.rousset@gmail.com>
2022-12-10 13:09:48 +0100

    day 10

---
+++ 2022/day10a.py
@@ -0,0 +1,41 @@
+import aocutils
+
+instructions = aocutils.read_lines("./2022/inputs/day10.txt")
+
+cycles_of_interest = [20, 60, 100, 140, 180, 220]
+signal_sum = 0
+x = 1
+cycle = 0
+
+def add_signal_strength() -> tuple[int, bool]:
+    if cycle == cycles_of_interest[0]:
+        signal = x * cycle
+        cycles_of_interest.pop(0)
+        #print(signal, cycle, x)
+        if len(cycles_of_interest) == 0:
+            return signal, True
+        return signal, False
+    return 0, False
+
+for instruction in instructions:
+    stop = False
+    signal = 0
+    #print(instruction)
+    if instruction == "noop":
+        cycle += 1
+        signal, stop = add_signal_strength()
+    elif instruction.startswith("addx"):
+        v = int(instruction[5:])
+        cycle += 1
+        signal, stop = add_signal_strength()
+        signal_sum += signal
+        if stop:
+            break
+        cycle += 1
+        signal, stop = add_signal_strength()
+        x += v 
+    signal_sum += signal
+    if stop:
+        break
+
+print(signal_sum)
---
+++ 2022/day10b.py
@@ -0,0 +1,33 @@
+import aocutils
+
+instructions = aocutils.read_lines("./2022/inputs/day10.txt")
+
+x = 1
+cycle = 0
+
+display = []
+
+def draw(pixel, sprite_pos):
+    px = (pixel - 1) % 40
+    if sprite_pos - 1 <= px <= sprite_pos + 1:
+        display.append("#")
+    else:
+        display.append(".")
+
+for instruction in instructions:
+    if instruction == "noop":
+        cycle += 1
+        draw(cycle, x)
+    elif instruction.startswith("addx"):
+        v = int(instruction[5:])
+        cycle += 1
+        draw(cycle, x)
+        cycle += 1
+        draw(cycle, x)
+        x += v
+
+print(cycle)
+
+while len(display) > 0:
+    print("".join(display[:40]))
+    display = display[40:]
