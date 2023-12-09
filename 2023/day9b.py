4692bcbaa3878ab1bdced681e83f3725c6c2b269
Niklas Rousset <niklas.rousset@gmail.com>
2023-12-09 08:46:54 +0100

    day 9

---
+++ 2023/day9b.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+
+def next_val(reading: list[int]):
+    diffs = [reading]
+    while not all(diff == 0 for diff in diffs[-1]):
+        new_diff = []
+        for i in range(1, len(diffs[-1])):
+            new_diff.append(diffs[-1][i - 1] - diffs[-1][i])
+        diffs.append(new_diff)
+    diffs[-1].append(0)
+    for i in range(len(diffs) - 2, -1, -1):
+        diffs[i].append(diffs[i][-1] - diffs[i + 1][-1])
+
+    return diffs[0][-1]
+
+
+readings = [
+    list(map(int, x.split(" ")))[::-1]
+    for x in Path("2023/inputs/day9.txt").read_text().splitlines()
+]
+
+extrapolations = list(map(next_val, readings))
+print(sum(extrapolations))
